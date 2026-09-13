9c6f16cebbc754d4b2544816987bdec8b4b0f225
pcmcdiary-hue <pcmcdiary@gmail.com>
2026-09-13 15:58:55 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://pcmcdiary.com',
   output: 'static',
   build: {
     format: 'directory'
-  }
+  },
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/404'),
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    })
+  ]
 });
